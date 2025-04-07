c0ae34a1f6e75f1edb393c60b1f66d3294c6bf45
gabrcastro <gabrielcastromail@gmail.com>
2025-04-07 08:34:16 -0300

    fix: hydration error isMobile

--- src/components/page-default.tsx
+++ src/components/page-default.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import CookiesComponent from "./cookies";
 
 interface IPageDefaultProps {
--- src/hooks/is_mobile.hook.tsx
+++ src/hooks/is_mobile.hook.tsx
@@ -1,25 +1,20 @@
+"use client";
+
 import { useEffect, useState } from "react";
 
 const useIsMobile = (): boolean => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Função que verifica o tamanho da tela
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
-
-    // Adiciona o listener para o evento de resize
-    window.addEventListener("resize", handleResize);
-
-    // Chama a função uma vez no início para garantir o estado inicial correto
     handleResize();
-
-    // Remove o listener quando o componente é desmontado
+    window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []); // Array de dependências vazio, pois só queremos rodar isso na montagem/desmontagem
+  }, []);
 
   return isMobile;
 };
